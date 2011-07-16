8d35ccd4092573ac7ce30f1fc622bab049606fa4
Kris Craig <kris.craig@gmail.com>
2011-07-16 03:56:55 -0700

    Added basic BFG handling settings.

--- config.phplogin.php
+++ config.phplogin.php
@@ -21,8 +21,22 @@ $phplogin_passhash = "sha512";  // Currently supported:  md5, sha1, sha256, sha5
  * sites.  The disadvantage, however, is that this will create an additional load (albeit 
  * a tiny one) on the server, potentially causing increased lag.  If you're trying to 
  * maximize performance, it is recommended that you disable this feature.
+ * 
+ * --Kris
  */
 $phplogin_ping_db = TRUE;
+
+/* How many consecutive failed login attempts on a user before image verification required (needs phpMeow or CAPTCHA).  --Kris */
+$phplogin_failures_verif_threshold = 5;
+
+/* How many consecutive failed login attempts on a user before that session is blocked from logging in.  --Kris */
+$phplogin_failures_ban_threshold = 100;
+
+/* If a session and/or IP is banned from logging in, the ban will expire in this many seconds.  --Kris */
+$phplogin_failures_ban_timeout = pow( 60, 2 ) * 24;
+
+/* If enabled, ban and verification thresholds will be cross-referenced against user's IP address.  Use with caution!  --Kris */
+$phplogin_failures_use_ip = FALSE;
 
 /* Do not modify or remove this!  --Kris */
 require( "config.init.phplogin.php" );
