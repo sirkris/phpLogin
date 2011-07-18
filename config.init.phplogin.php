3d77a5b70d04543466f53004dee12cc12f31a7a4
Kris Craig <kris.craig@gmail.com>
2011-07-18 03:20:53 -0700

    Fixes to structural defects in SQL classes.  Not sure why I didn't just use a construct to begin with.

--- config.init.phplogin.php
+++ config.init.phplogin.php
@@ -8,12 +8,3 @@
  */
 require( "config.secure.phplogin.php" );
 require_once( $phplogin_sql_server . ".phplogin.class.php" );
-
-define( "RETURN_NULL", 0 );
-define( "RETURN_RESULTS", 1 );  // Default.
-define( "RETURN_AFFECTEDROWS", 2 );
-define( "RETURN_NUMROWS", 3 );
-define( "RETURN_OBJECT", 4 );
-
-define( "SQL_AND", TRUE );
-define( "SQL_OR", FALSE );
