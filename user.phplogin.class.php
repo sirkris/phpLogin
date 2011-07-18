d004afb46ea0da2c9fbda2981fc34afe2ae366e1
Kris Craig <kris.craig@gmail.com>
2011-07-18 04:34:31 -0700

    Finished basic user functions/settings.  Haven't decided yet whether authentication should go here or in its own class.  Will address in separate branch.

--- user.phplogin.class.php
+++ user.phplogin.class.php
@@ -17,20 +17,21 @@ class phplogin_user
 			|| !is_numeric( $_SESSION["phplogin_userid"] ) 
 			|| $_SESSION["phplogin_userid"] <= 0 )
 		{
-			/* Don't use session_destroy or session_unset because host script might have unrelated session data already.  --Kris */
-			foreach ( $_SESSION as $skey => $ignore )
-			{
-				if ( strcmp( substr( $skey, 0, 9 ), "phplogin_" ) == 0 )
-				{
-					unset( $_SESSION[$skey] )
-				}
-			}
+			$this->clear_session();
 			
 			/* Check the database to rebuild session data, if possible.  --Kris */
 			if ( isset( session_id() ) )
 			{
 				$this->populate_session();
 			}
+		}
+		else if ( time() >= $_SESSION["phplogin_sessiontimeout"] )
+		{
+			$this->populate_session();
+		}
+		else
+		{
+			$this->update_lastaction();
 		}
 		
 		/* Populate common data from session for convenience.  --Kris */
@@ -59,13 +60,51 @@ class phplogin_user
 		return $sql->query( "select $select from phplogin_users where phpsessid = ?", array( $sql->addescape( session_id() ) ) );
 	}
 	
+	function clear_session()
+	{
+		/* Don't use session_destroy or session_unset because host script might have unrelated session data already.  --Kris */
+		foreach ( $_SESSION as $skey => $ignore )
+		{
+			if ( strcmp( substr( $skey, 0, 9 ), "phplogin_" ) == 0 )
+			{
+				unset( $_SESSION[$skey] )
+			}
+		}
+	}
+	
 	function populate_session()
 	{
+		require( "config.phplogin.php" );
+		
+		$this->update_lastaction();
+		
 		$_SESSION["phplogin_userdata"] = $this->load_data();
+		
+		/* If user has been logged-out, banned, etc; clear the session data.  --Kris */
+		if ( $_SESSION["userdata"]["status"] <= 0 || $_SESSION["userdata"]["loggedon"] != 1 )
+		{
+			$this->clear_session();
+			return;
+		}
 		
 		foreach ( $_SESSION["phplogin_userdata"] as $sukey => $suval )
 		{
 			$_SESSION["phplogin_" . $sukey] = $suval;
+		}
+		
+		$_SESSION["phplogin_sessiontimeout"] = time() + $phplogin_session_repopulate();
+	}
+	
+	/* Update the last action timestamp in the database (if enabled).  --Kris */
+	function update_lastaction()
+	{
+		require( "config.phplogin.php" );
+		
+		if ( $phplogin_ping_db == TRUE && isset( $_SESSION["phplogin_userid"] ) )
+		{
+			$sql = new phplogin_sql();
+			
+			$sql->query( "update phplogin_users set lastaction = ? where phpsessid = ?", array( strval( time() ), $sql->addescape( session_id() ) ), RETURN_NULL );
 		}
 	}
 }
