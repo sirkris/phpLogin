06d02928cf95063e8dcc52443bbca073f6a321e1
Kris Craig <kris.craig@gmail.com>
2011-07-19 03:52:06 -0700

    Function declarations.

---
+++ authenticate.phplogin.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class phplogin_authenticate
+{
+	/* Validate username/password.  --Kris */
+	function login( $username, $password )
+	{
+		
+	}
+	
+	/* Determine if session data indicates a logged-in user.  --Kris */
+	function session()
+	{
+		
+	}
+	
+	/* Determine if the user is allowed to login (status > 0).  --Kris */
+	function allowed()
+	{
+		
+	}
+	
+	/* Determine if the user has moderator privileges.  --Kris */
+	function is_moderator()
+	{
+		
+	}
+	
+	/* Determine if the user has admin privileges.  --Kris */
+	function is_admin()
+	{
+		
+	}
+	
+	/* Determine if the user has superuser privileges.  --Kris */
+	function is_superuser()
+	{
+		
+	}
+}
