040a71be84ef2793af9a468c423a8e462d858a20
Kris Craig <kris.craig@gmail.com>
2011-07-27 09:02:59 -0700

    Edit user frontend script.

---
+++ edit_user.frontend.phplogin.php
@@ -0,0 +1,33 @@
+<?php
+
+require( "config.phplogin.php" );
+
+$user = new phplogin_user();
+
+/* If the userid is invalid/banned/etc, redirect to view_profile and let that script handle it.  --Kris */
+if ( !isset( $user->userdata ) || !isset( $user->userdata["status"] ) || !isset( $user->userid ) || $user->userdata["status"] < 1 )
+{
+	header( "Location: view_profile.frontend.phplogin.php?userid=" . $user->userid . "&errmsg=Unable to edit user!" );
+	die();
+}
+
+$phplogin_templates = new phplogin_templates();
+
+$phplogin_templates->set( "title", "Edit Profile" );
+$phplogin_templates->display( "header" );
+$phplogin_templates->clear();
+
+?><div class="phplogin_editusercontainerdiv"><?php
+
+$phplogin_templates->set( "action", "edit_user.backend.phplogin.php" );
+$phplogin_templates->set( "submit", "Save Changes" );
+$phplogin_templates->set( "errmsg", ( isset( $_POST["phplogin_errmsg"] ) ? $_POST["phplogin_errmsg"] : NULL ) );
+$phplogin_templates->set( "username", $username );
+$phplogin_templates->set( "email", $email );
+
+$phplogin_templates->display( "edit_user" );
+
+?></div><?php
+
+$phplogin_templates->clear();
+$phplogin_templates->display( "footer" );
